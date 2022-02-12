2d7df99d609a9ad524cdfaba5ab834dc976567fd
taya-nicholas <taya-nicholas@outlook.com>
2022-02-13 11:55:22 +1300

    Set autocomplete onpress to store location data in redux

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,8 +4,11 @@ import tw from "tailwind-react-native-classnames";
 import NavOptions from "../components/NavOptions";
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 import { GOOGLE_MAPS_APIKEY } from "@env";
+import { useDispatch } from "react-redux";
+import { setDestination, setOrigin } from "../slices/navSlice";
 
 const HomeScreen = () => {
+  const dispatch = useDispatch();
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
       <View style={tw`p-5`}>
@@ -20,6 +23,33 @@ const HomeScreen = () => {
           }}
         />
         <GooglePlacesAutocomplete
+          styles={{
+            container: {
+              flex: 0,
+            },
+            textInput: {
+              fontSize: 18,
+            },
+          }}
+          onPress={(data, details = null) => {
+            // console.log(data, details);
+            dispatch(
+              setOrigin({
+                location: details.geometry.location,
+                description: data.description,
+              })
+            );
+
+            dispatch(setDestination(null));
+          }}
+          enablePoweredByContainer={false}
+          fetchDetails={true}
+          minLength={2}
+          query={{
+            key: GOOGLE_MAPS_APIKEY,
+            language: "en",
+            components: "country:nz",
+          }}
           placeholder="Where From?"
           nearbyPlacesAPI="GooglePlacesSearch"
           debounce={400}
