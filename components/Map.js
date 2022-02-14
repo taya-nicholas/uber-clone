1406eb8615b4edc6a831f88903c9ad8348cc45b4
taya-nicholas <taya-nicholas@outlook.com>
2022-02-15 10:17:23 +1300

    Fixed bug where map didn't fit origin and destination to screen

--- components/Map.js
+++ components/Map.js
@@ -38,8 +38,31 @@ const Map = () => {
       ref={mapRef}
       style={tw`flex-1`}
       mapType="standard"
-      onMapReady={() => {
-        this.mapRef.fitToSuppliedMarkers(["origin", "destination"]);
+      onLayout={() => {
+        console.log("Map ready - new");
+        let markers = [];
+        let placeMarker1 = {
+          latitude: origin.location.lat,
+          longitude: origin.location.lng,
+          title: "origin",
+          key: 1,
+        };
+        let placeMarker2 = {
+          latitude: destination.location.lat,
+          longitude: destination.location.lng,
+          title: "destination",
+          key: 2,
+        };
+        markers.push(placeMarker1);
+        markers.push(placeMarker2);
+        console.log(markers);
+        mapRef.current.fitToCoordinates(markers, {
+          edgePadding: { top: 50, right: 10, bottom: 10, left: 10 },
+          animated: false,
+        });
+        // mapRef.current.fitToSuppliedMarkers(["origin", "destination"], {
+        //   edgePadding: { top: 50, right: 50, bottom: 50, left: 50 },
+        // });
       }}
       initialRegion={{
         latitude: origin.location.lat,
